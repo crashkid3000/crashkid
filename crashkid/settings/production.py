63cf53e5e58d472398dc1a4cbf7b15e4ed92c9a2
crashkid3000 <28297180+crashkid3000@users.noreply.github.com>
2020-02-20 12:24:51 +0100

    production.py: Loads SECRET_KEY, ALLOWED_HOSTS from os

--- crashkid/settings/production.py
+++ crashkid/settings/production.py
@@ -1,8 +1,12 @@
 from .base import *
-
+import os
 DEBUG = False
+
+SECRET_KEY = os.environ['DJANGO_SECRET_KEY']
+ALLOWED_HOSTS = [os.environ['DJANGO_ALLOWED_HOST_NAME']]
 
 try:
     from .local import *
 except ImportError:
-    pass
+    print('Local settings were not found.')
+
